d551f11b402c23b0ee2e878edcdc6561d4571903
holiday526 <48387601+holiday526@users.noreply.github.com>
2020-10-31 17:31:54 +0800

    sb-admin-2 setup done

---
+++ resources/views/partials/foot_script.blade.php
@@ -0,0 +1,15 @@
+<!-- Bootstrap core JavaScript-->
+{{--<script src="vendor/jquery/jquery.min.js"></script>--}}
+<script src="{{ asset('js/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+{{--<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>--}}
+
+<!-- Core plugin JavaScript-->
+{{--<script src="vendor/jquery-easing/jquery.easing.min.js"></script>--}}
+<script src="{{ asset('js/jquery-easing/jquery.easing.min.js') }}"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+
+<!-- Page level plugins -->
+<script src="{{ asset('js/chart.js/Chart.min.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () { return view('index'); });
 });
 
 Auth::routes();
